34850e6d08c47dcf7e3186f5a9043cda5dc8a4b6
Bhalachandra <bhalapatil1@gmail.com>
2025-04-17 23:53:20 -0400

    stt1

--- worker.py
+++ worker.py
@@ -5,8 +5,25 @@ openai_client = OpenAI()
 
 
 def speech_to_text(audio_binary):
-    return None
+    base_url = "https://sn-watson-stt.labs.skills.network"
+    api_url=base_rl+ '/speech-to-text/api/v1/recognize'
 
+    params = {
+
+        'model' : 'en-US_Multimedia',
+    }
+
+    #set up the body of HTTP request
+
+    body = audio_binary
+    response = requests.post(api_url,params=params,data=audio_binary).json()
+
+    test ='null'
+    while bool(response.get('results')):
+        print('speech to text response', respone)
+        test = response.get('results').pop().get('alternatives').pop().get('transcript')
+        print('recognised text ' : text)
+        return text
 
 def text_to_speech(text, voice=""):
     return None
