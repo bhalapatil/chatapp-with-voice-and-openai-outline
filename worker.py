c21825206e44eaa3e05115e134356609846740df
Bhalachandra <bhalapatil1@gmail.com>
2025-04-18 00:45:53 -0400

    openai call

--- worker.py
+++ worker.py
@@ -30,4 +30,25 @@ def text_to_speech(text, voice=""):
 
 
 def openai_process_message(user_message):
-    return None
+    # set the prompt for the OpenAI Api
+    prompt ="Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations "
+
+    #call the OpenAI Api to process the prompt
+    openai_response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role" : "system", "content" : promt},
+                {"role" : "user" , "content" : user_message}
+        ],
+        max_tokens=4000
+    )
+
+    print("openai response",openai_response)
+
+    #parse the response to get the response message from the pr
+    response_text = openai_response.choices[0].message.content
+    return response_text
+
+
+    
+
